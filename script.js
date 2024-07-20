6ed65797ef5bce6c274f401defb808f620d7d400
AnderTJ14 <134516974+AnderTJ14@users.noreply.github.com>
2024-07-20 15:12:56 -0500

    script.js

--- script.js
+++ script.js
@@ -34,7 +34,11 @@ async function loadModel() {
 captureButton.addEventListener('click', () => {
     context.drawImage(video, 0, 0, canvas.width, canvas.height);
     const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-    const imageTensor = tf.browser.fromPixels(imageData).resizeNearestNeighbor([100, 100]).toFloat().expandDims();
+    let imageTensor = tf.browser.fromPixels(imageData).resizeNearestNeighbor([100, 100]).toFloat().expandDims();
+
+    // Convertir la imagen a escala de grises
+    imageTensor = tf.image.rgbToGrayscale(imageTensor).expandDims(3);
+
     predict(imageTensor);
 });
 
@@ -45,25 +49,22 @@ switchCameraButton.addEventListener('click', () => {
 
 async function predict(imageTensor) {
     try {
-        // Convertir la imagen a escala de grises si el modelo espera imágenes en escala de grises
-        const grayImageTensor = imageTensor.mean(3).expandDims(3); // Promediar los canales de color y añadir una dimensión de canal
+        // Verificar que la imagen tenga las dimensiones correctas
+        console.log('Tensor de imagen (gris):', imageTensor.shape);
 
-        // Asegúrate de que el tensor de imagen tenga las dimensiones correctas
-        console.log('Tensor de imagen (gris):', grayImageTensor.shape);
+        // Realizar la predicción
+        const prediction = model.predict(imageTensor);
 
-        // Realiza la predicción
-        const prediction = model.predict(grayImageTensor);
-
-        // Imprime los resultados de la predicción
+        // Imprimir los resultados de la predicción
         prediction.print(); // Esto te permitirá ver los valores de predicción en la consola
 
-        // Obtén la clase con la mayor probabilidad
+        // Obtener la clase con la mayor probabilidad
         const predictedClass = prediction.argMax(1).dataSync()[0];
         console.log('Clase predicha:', predictedClass);
 
         // Asegúrate de que la clase sea válida
         const classes = ['Perro', 'Gato'];
-        predictionSpan.textContent = classes[predictedClass];
+        predictionSpan.textContent = `Predicción: ${classes[predictedClass]}`;
     } catch (error) {
         console.error('Error en la predicción:', error);
         predictionSpan.textContent = 'Error en la predicción';
